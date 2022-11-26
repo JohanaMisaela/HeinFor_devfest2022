86d78be891ddcbc4b03781fea7a27a4668da553c
josiaYvan <josiayvan@gmail.com>
2022-11-27 01:38:27 +0300

    client ok

---
+++ backend/frontend/src/components/Log/SignUpForm.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import axios from "axios";
+import SignInForm from "./SignInForm";
+const SignUpForm = () => {
+  const [formSubmti, setFormSubmti] = useState(false);
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [controlPassword, setControlPassword] = useState("");
+
+  const handleRegIster = async (e) => {
+    e.preventDefault();
+    const terms = document.getElementById("terms");
+    const pseudoError = document.querySelector(".pseudo.error");
+    const emailError = document.querySelector(".email.error");
+    const passwordError = document.querySelector(".password.error");
+    const passwordConfrimError = document.querySelector(
+      ".password-confirm.error"
+    );
+    const termsError = document.querySelector(".terms.error");
+
+    passwordConfrimError.innerHTML = "";
+    termsError.innerHTML = "";
+
+    if (password !== controlPassword || !terms.checked) {
+      if (password !== controlPassword)
+        passwordConfrimError.innerHTML =
+          "Les mots de passe ne correspondent pas!";
+
+      if (!terms.checked)
+        termsError.innerHTML = "Veuillez valider les conditions générales";
+    } else {
+      await axios({
+        method: "post",
+        url: `${process.env.REACT_APP_API_URL}api/user/register`,
+        data: {
+          pseudo,
+          email,
+          password,
+        },
+      })
+        .then((res) => {
+          console.log(res);
+          if (res.data.errors) {
+            pseudoError.innerHTML = res.data.errors.pseudo;
+            emailError.innerHTML = res.data.errors.email;
+            passwordError.innerHTML = res.data.errors.password;
+          } else {
+            setFormSubmti(true);
+          }
+        })
+        .catch((err) => console.log(err.response.data));
+    }
+  };
+  return (
+    <>
+      {formSubmti ? (
+        <>
+          <SignInForm />
+          <span></span>
+          <h4 className="success">
+            {" "}
+            Enregistrement réussi, veuillez vous connecter{" "}
+          </h4>
+        </>
+      ) : (
+        <form action="" onSubmit={handleRegIster}>
+          <label htmlFor="pseudo">Pseudo</label>
+          <div className="pseudo error"></div>
+          <br />
+          <input
+            type="text"
+            name="pseudo"
+            id="pseudo"
+            onChange={(e) => setPseudo(e.target.value)}
+            value={pseudo}
+          />
+          <br />
+          <div className="email error"></div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <br />
+          <div className="password error"></div>
+          <label htmlFor="password">Mot de passe</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+          <br />
+          <div className="password-confirm error"></div>
+          <label htmlFor="password-conf">Confirmer le mot de passe</label>
+          <br />
+          <input
+            type="password"
+            name="password"
+            id="password-conf"
+            onChange={(e) => setControlPassword(e.target.value)}
+            value={controlPassword}
+          />
+          <br />
+          <div className="terms error"></div>
+          <input type="checkbox" name="terms" id="terms" />
+          <label htmlFor="terms">
+            J'accepte les{" "}
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              conditions generales
+            </a>{" "}
+          </label>
+          <br />
+          <input type="submit" value="Valider inscription" />
+        </form>
+      )}
+    </>
+  );
+};
+
+export default SignUpForm;
