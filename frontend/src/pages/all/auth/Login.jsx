a64c6fd18ce667b600c1baf740ccc3207c92175d
RANAIVOARISON MIONJA <mionjaran2005@gmail.com>
2022-11-26 15:58:52 -0700

    login

--- frontend/src/pages/all/auth/Login.jsx
+++ frontend/src/pages/all/auth/Login.jsx
@@ -82,12 +82,12 @@ function Login() {
                              onChange={(e) => setPassword(e.target.value)}/>
                           </div>
         
-                            {/* <div className="pt-1 mb-4">
+                            <div className="pt-1 mb-4">
                               <button className="btn btn-dark btn-lg btn-block" type="submit" name="connect">Se connecter</button>
-                            </div> */}
-                          <div className="pt-1 mb-4">
-                              <a className="btn btn-dark btn-lg btn-block" href='/dashboard'>Se connecter</a>
                             </div>
+                          {/* <div className="pt-1 mb-4">
+                              <a className="btn btn-dark btn-lg btn-block" href='/dashboard'>Se connecter</a>
+                            </div> */}
 
                           <a className="small text-muted" href="#!">Mot de passe oublié?</a>
                           <p className="mb-5 pb-lg-2" style={{color: "#393f81"}}>Pas de compte?
