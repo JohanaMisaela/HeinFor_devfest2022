7707bcfcd08c83b06d3d3b809aa092029848fb09
RANAIVOARISON MIONJA <mionjaran2005@gmail.com>
2022-11-26 19:04:24 -0700

    all

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const corsOption = {
   methods: "GET,PUT,POST,PATCH,DELETE,HEAD",
   prefLightContinue: false,
 };
-app.use(cors(corsOption));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
