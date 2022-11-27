b538b43a257e4c7b8f9c7fcd1d4a005bc80f1c46
RANAIVOARISON MIONJA <mionjaran2005@gmail.com>
2022-11-27 11:59:42 -0700

    plaintes

--- backend/frontend/src/pages/admins/plaintes/plainte_table.jsx
+++ backend/frontend/src/pages/admins/plaintes/plainte_table.jsx
@@ -114,10 +114,10 @@ function Plainte_table() {
   return (
     <div className="container p-3">
       <br/>
-      <br/>
+      
       <div style={{ margin: "35px"}} className="p-3">
       {
-        (plaintes.length>0 && users.length>0 ) &&
+          (plaintes.length>0 && users.length>0 ) &&
         <MaterialTable
           columns={colums}
           data={plaintes}
@@ -144,11 +144,13 @@ function Plainte_table() {
             },
           }}
         />
+        
         }
         {
           plaintes.length === 0  && 'Loading...'
         }
       </div>
+      <a  href="complaints/add">Ajouter</a>
     </div>
   );
 }
